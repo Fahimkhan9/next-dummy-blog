d6850ee52cfc32b1c022a19ea0a0f8f85f30c799
Fahimkhan9 <61534271+Fahimkhan9@users.noreply.github.com>
2021-07-20 00:46:05 +0600

    fallback:true and 404 page for not existing id

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -10,62 +10,52 @@ const paths = data.map(blog => {
 })
 return{
     paths,
-    fallback:false
+    fallback:true
 }
 
 }
 
 export const getStaticProps = async (context) => {
     const id = context.params.id;
-    const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id);
+    const res = await fetch('https://jsonplaceholder.typicode.com/photos/' + id);
     const data = await res.json();
+
+    if(!data.id){
+      return{
+        notFound: true
+      }
+    }
   
     return {
       props: { singleblog: data }
     }
   }
-function Datils({singleblog}) {
+function Details({singleblog}) {
 console.log(singleblog)
+const router=useRouter()
+if(router.isFallback){
+  return(
+    <div className="bg-gray-500">
+      <h1
+      className='font-semibold'
+      >Loading....</h1>
+    </div>
+  )
+}
     return (
         <div>
             <Navigation />
           <div className='flex justify-center align-center w-auto' >
-          <div className="p-8 bg-white border rounded shadow-sm">
-                        
-                        <p
-                              
-                              title={singleblog.name}
-                              className="inline-block cursor-pointer mb-3 text-2xl font-bold leading-5 text-black transition-colors duration-200 hover:text-deep-purple-accent-400"
-                            >
-                              {singleblog.name}
-                            </p>
-                       
-                            <p className="mb-5 text-gray-700">
-                              {singleblog.email}
-                            </p>
-                            <div className="flex items-center">
-                              <a href="/" aria-label="Author" title="Author" className="mr-3">
+        
+                           
+                            
                                 <img
-                                  src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-                                  alt="avatar"
+                                  src={singleblog.url}
+                                  alt={singleblog.title}
                                   className="object-cover w-10 h-10 rounded-full shadow-sm"
                                 />
-                              </a>
-                              <div>
-                                <a
-                                  href="/"
-                                  aria-label="Author"
-                                  title="Author"
-                                  className="font-semibold text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                                >
-                                  Vasile Melinte
-                                </a>
-                                <p className="text-sm font-medium leading-4 text-gray-600">
-                                  Author
-                                </p>
-                              </div>
-                            </div>
-                          </div>
+                          
+                     
                
           </div>
              
@@ -73,4 +63,4 @@ console.log(singleblog)
     )
 }
 
-export default Datils
+export default Details
